c06295f9ca484876e2e1e0123c87317934a93d0b
xy z <xy z>
2016-06-06 16:38:17 +0800

    “创建项目文件目录

---
+++ nodejs/server.js
@@ -0,0 +1,7 @@
+var http = require("http");
+
+var server = http.createServer(function(request,response){
+  response.writeHead(200,{"Content-Type":"text/plain"});
+  response.write("hello,world");
+  response.end();
+}).listen(8888);
